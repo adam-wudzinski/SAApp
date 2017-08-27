95e806d25bc90f46ea31d54e881efe1cc971b552
Yarlaxle <adaswudzinski94@gmail.com>
2017-08-27 17:09:21 +0200

    Project init

---
+++ src/main/java/com/gabaya/saapp/SaappApplication.java
@@ -0,0 +1,12 @@
+package com.gabaya.saapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gabaya/saapp/SaappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gabaya.saapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
